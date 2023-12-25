f867d49de912191633ab7405228db8a5580d1c88
kg73 <kevin@gysberg.me>
2023-12-25 17:37:53 -0600

    Added CLI project and stubbed out modem class

---
+++ BellModem/Modem.cs
@@ -0,0 +1,13 @@
+﻿namespace BellModem;
+public  class Modem
+{
+	public byte[] Modulate(object data)
+	{
+		throw new NotImplementedException();
+	}
+
+	public object Demodulate(byte[] data)
+	{
+		throw new NotImplementedException();
+	}
+}
