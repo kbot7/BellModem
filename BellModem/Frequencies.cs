53e2d83257b88dbf59f982892d3398b70fb67e9d
kg73 <kevin@gysberg.me>
2023-12-25 17:32:26 -0600

    Added constants for Bell 103 Mark and Space for Originate and Answer modes

---
+++ BellModem/Frequencies.cs
@@ -0,0 +1,15 @@
+﻿namespace BellModem;
+
+public class Frequencies
+{
+	public class Originate
+	{
+		public const double MARK = 1270.0;
+		public const double SPACE = 1070.0;
+	}
+	public class Answer
+	{
+		public const double MARK = 2225.0;
+		public const double SPACE = 2025.0;
+	}
+}
